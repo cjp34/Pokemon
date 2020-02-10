6986acdb6fc72c2a86f217818a7a3fc8dbf9ee1d
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-10 16:32:10 -0500

    Starting to go through types

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require 'json'
 require_all 'apps/models'
 
 poke_data = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
+Pokemon.destroy_all
 
 def rest_and_parse(url)
     rest_data = RestClient.get(url)
@@ -17,6 +18,14 @@ def poke_name
     poke_data = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
     poke_data_better = rest_and_parse(poke_data)
     poke_data_better.map { |nam| nam["name"]}
+end
+
+def poke_type
+    poke_data = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
+    poke_data_better = rest_and_parse(poke_data)
+
+    poke_url = poke_data_better.map { |mov| mov["url"]}
+    binding.pry
 end
 
 poke_name.each { |name| Pokemon.create(species: name)}
